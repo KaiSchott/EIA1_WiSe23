a79fb1e08e47eea8cef70fdfad94e9176e579360
Kai Schott <schott-kai@web.de>
2023-12-04 13:00:45 +0100

    EIAYAYAYA

---
+++ 06/script.js
@@ -0,0 +1,17 @@
+
+let start = prompt("Schreibe los um anzufangen");
+
+if(start =="los"){
+let wohin =prompt("Du stehts auf einem Hügel und kannst dich entscheiden ob du kletterst oder läufst um den 9 Uhr Zug zu bekommen. Schreibe klettern oder laufen um dich zu entscheiden.")
+if(wohin == "klettern"){
+    let aua = prompt("Du bist ausgerutscht, leider hast du dich verltzt und kannst dich kaum noch vortbewegen. Du verpasst den Zug")
+}
+else if(wohin == "laufen"){
+    let schneller = prompt("Du kommst rechtzeig an aber der Zug kommt 30 Minuten später als geplant")
+
+}
+
+
+}
+
+
